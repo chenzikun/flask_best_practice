0d1dfa044f3cd73bfb98cea9c55f25b76684bddb
czk <chenzikunczk@gmail.com>
2017-06-27 10:20:28 +0800

    增加单元测试

---
+++ test/api.py
@@ -0,0 +1,9 @@
+import unittest
+import requests
+
+class ApiTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
---
+++ test/demo.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import skip
+
+from selenium import webdriver
+
+
+class DemoTest(unittest.TestCase):
+    def setUp(self):
+        """Hook method for setting up the test fixture before exercising it."""
+        pass
+
+    def tearDown(self):
+        """Hook method for deconstructing the test fixture after testing it."""
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
