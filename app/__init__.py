aa5f84e92a6ee59c9c46056d71d4d268990f9bc3
czk <chenzikunczk@gmail.com>
2017-06-30 12:53:01 +0800

    current_app里增加celery

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,40 @@
 from flask import Flask
+from celery import Celery
 
 
 class Application(Flask):
     def __init__(self):
         super(Application, self).__init__(__name__)
+        self.config.update(
+            CELERY_BROKER_URL='redis://127.0.0.1:6379',
+            CELERY_RESULT_BACKEND='redis://127.0.0.1:6379'
+        )
+        self.celery = self.make_celery()
 
     def _init_blueprint(self, *blueprints):
         for blueprint in blueprints:
             self.register_blueprint(blueprint)
 
+    def make_celery(self):
+        celery = Celery(self.import_name, broker=self.config['CELERY_BROKER_URL'])
+        celery.conf.update(self.config)
+        TaskBase = celery.Task
+
+        class ContextTask(TaskBase):
+            abstract = True
+
+            def __call__(self, *args, **kwargs):
+                with self.app_context():
+                    return TaskBase.__call__(self, *args, **kwargs)
+
+        celery.Task = ContextTask
+        return celery
+
 
 def create_app():
     app = Application()
 
-    from app.homepage.view import homepage_bp
+    from app.views.homepage import homepage_bp
     from app.apis.api import api_bp
     blueprints = [homepage_bp, api_bp]
     app._init_blueprint(*blueprints)
--- run.py
+++ run.py
@@ -1,31 +1,11 @@
 from app import create_app
-from celery import Celery
 
 app = create_app()
 
 
-def make_celery(app):
-    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
 
 
-app.config.update(
-    CELERY_BROKER_URL='redis://localhost:6379',
-    CELERY_RESULT_BACKEND='redis://localhost:6379'
-)
 
-celery = make_celery(app)
 
 
 # @celery.task()
