3907ba104df2cd03bbb4187911c4ac1e998b3b3f
czk <chenzikunczk@gmail.com>
2017-07-01 00:02:19 +0800

    增加扩展

---
+++ app/ext/__init__.py
@@ -0,0 +1,27 @@
+from flask import current_app
+
+
+class Extension(object):
+    def __init__(self, app=None):
+        self._app = app
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions.update({'extension': self, })
+
+    @property
+    def app(self):
+        return self._app or current_app
+
+    def config(self, key, default=None):
+        key = 'EXTENSION_{}'.format(key.upper())
+        if default:
+            return self.app.config.get(key, default)
+        return self.app.config[key]
+
+    @property
+    def namespace(self):
+        return self.config('namespace', 'extension')
